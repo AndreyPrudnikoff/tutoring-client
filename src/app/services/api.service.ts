5526aa8febbe6e8e55fcaba7c184c3f2ead67eba
andrii <a.prudniks@gmail.com>
2022-10-24 23:13:21 +0300

    first

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+type Method = 'get' | 'post' | 'put' | 'patch' | 'delete';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  baseUrl = 'http://localhost:3000/api'
+
+  constructor(private http: HttpClient) {
+http.get()
+  }
+  basicOperation(method: Method, url: string) {
+    return this.http[method](`${this.baseUrl}/${url}`)
+  }
 }
