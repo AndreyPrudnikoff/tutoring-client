a685ed70f3f9bd8f9887d7bb136e5a5e8a047f46
Andrei <a.prudniks@gmail.com>
2022-10-26 23:53:39 +0300

    login

--- src/app/pages/start-page/start-page.component.ts
+++ src/app/pages/start-page/start-page.component.ts
@@ -1,6 +1,29 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ApiService} from "../../services/api.service";
+
+const formFields = [{
+  type: 'text',
+  text: 'Your first name',
+  field: 'first_name'
+}, {
+  type: 'text',
+  text: 'Your last name',
+  field: 'last_name'
+}, {
+  type: 'text',
+  text: 'Your phone',
+  field: 'phone'
+}, {
+  type: 'text',
+  text: 'Your email',
+  field: 'email'
+}, {
+  type: 'password',
+  text: 'Your password',
+  field: 'reg_password'
+}
+]
 
 @Component({
   selector: 'app-start-page',
@@ -10,12 +33,18 @@ import {ApiService} from "../../services/api.service";
 export class StartPageComponent implements OnInit {
 
   isLogin = true;
+  formFields = formFields;
   form: FormGroup = this.fb.group({
-    name: ['', Validators.required],
-    password: ['', Validators.required]
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+    role: false
   })
 
-  submit() {
+  register() {
+    this.api.operation('register', this.form.value)
+      .subscribe(console.log)
+  }
+  login() {
     this.api.operation('login', this.form.value)
       .subscribe(console.log)
   }
@@ -24,18 +53,24 @@ export class StartPageComponent implements OnInit {
     this.isLogin = !this.isLogin;
     if (this.isLogin) {
       this.form = this.fb.group({
-        name: ['', Validators.required],
-        password: ['', Validators.required]
-      })} else {
-        this.form = this.fb.group({
-          first_name: ['', Validators.required],
-          last_name: ['', Validators.required],
-          phone: ['', Validators.required],
-          email: ['', Validators.required],
-          role: [false, Validators.required],
-          reg_password: ['', Validators.required]
-        })
-      }
+        email: ['', Validators.required],
+        password: ['', Validators.required],
+        role: false
+      })
+    } else {
+      this.form = this.fb.group({
+        first_name: ['', Validators.required],
+        last_name: ['', Validators.required],
+        phone: ['', Validators.required],
+        email: ['', Validators.required],
+        role: false,
+        reg_password: ['', Validators.required]
+      })
+    }
+  }
+
+  log() {
+    console.log(this.form.get('role')?.value)
   }
 
   constructor(private fb: FormBuilder, private api: ApiService) {
